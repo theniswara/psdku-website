281df893237e98c46f8da17167c212d1142b5ef1
theniswara <daniswara.mdr@proton.me>
2025-11-10 19:15:50 +0700

    first commit

---
+++ springboot-backend/src/test/java/com/polinema/psdku/springboot_backend/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polinema.psdku.springboot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
