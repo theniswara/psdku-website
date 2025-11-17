e1a3fa74c4fb2888dfd3608b20eba24280f20382
theniswara <daniswara.mdr@proton.me>
2025-11-17 12:06:47 +0700

    add bidang keahlian

--- springboot-backend/src/main/java/com/polinema/psdku/springbootbackend/model/BidangKeahlian.java
+++ springboot-backend/src/main/java/com/polinema/psdku/springbootbackend/model/BidangKeahlian.java
@@ -1,5 +1,48 @@
 package com.polinema.psdku.springbootbackend.model;
 
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "bidang_keahlian")
 public class BidangKeahlian {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_keahlian")
+    private int idKeahlian;
+
+    @Column(name = "nama_keahlian", length = 200)
+    private String namaKeahlian;
+
+    @ManyToOne
+    @JoinColumn(name = "id_dosen")
+    @JsonIgnoreProperties("bidangKeahlian")
+    private Dosen dosen;
+
+    // getters & setters
+
+    public int getIdKeahlian() {
+      return idKeahlian;
+    }
+
+    public void setIdKeahlian(int idKeahlian) {
+      this.idKeahlian = idKeahlian;
+    }
+
+    public String getNamaKeahlian() {
+      return namaKeahlian;
+    }
+
+    public void setNamaKeahlian(String namaKeahlian) {
+      this.namaKeahlian = namaKeahlian;
+    }
+
+    public Dosen getDosen() {
+      return dosen;
+    }
+
+    public void setDosen(Dosen dosen) {
+      this.dosen = dosen;
+    }
 }
