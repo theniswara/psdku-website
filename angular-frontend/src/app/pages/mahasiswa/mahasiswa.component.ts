690d94f226d21dc3ddf44829bada357132abfed2
adindadianarini <adindarini29@gmail.com>
2026-01-08 13:35:15 +0700

    tambah mahasiswa

---
+++ angular-frontend/src/app/pages/mahasiswa/mahasiswa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mahasiswa',
+  standalone: true,
+  imports: [],
+  templateUrl: './mahasiswa.component.html',
+  styleUrl: './mahasiswa.component.css'
+})
+export class MahasiswaComponent {
+
+}
